c2b7b2faccc5651639b88d043f90649a92e47f06
talryan <nataliamryan@gmail.com>
2020-10-26 17:05:31 -0700

    commit tree 9400a0c0b1cc9d7d25510b93f2227a7363da7f58

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+
+dwarves.each_with_index do |name, index|
+puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+ 
+    
+
+require "pry"
+
+def summon_captain_planet (array)
+array.map { |w| w.capitalize + "!" }
+
+
+# binding.pry
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+cheddar_cheese.detect{ |cheese| cheese_types.include? (cheese)}
 end
